9f9619b8f19fee4529f29e03e7d0103bc2e56edf
SUDHANSHU GHOSH <111761979+sudhanshu9199@users.noreply.github.com>
2026-05-31 13:59:25 +0530

    Refactor miniMaxSum to use for...of loop
    Refactored miniMaxSum function to use for...of loop for better readability.

--- Mini-Max_Sum.js
+++ Mini-Max_Sum.js
@@ -3,12 +3,15 @@ question: Mini-Max Sum
 Link: https://www.hackerrank.com/challenges/mini-max-sum/problem
 ```
 
-let total = 0;
+// traditional for loop
+function miniMaxSum(arr) {
+    // Write your code here
+    let total = 0;
     let min = arr[0];
     let max = arr[0];
     for (let i = 0; i < arr.length; i++) {
         total += arr[i];
-        if (arr[i] < max) {
+        if (arr[i] < min) {
             min = arr[i];
         }
         if (arr[i] > max) {
@@ -18,3 +21,30 @@ let total = 0;
     let minSum = total - max;
     let maxSum = total - min;
     console.log(minSum, maxSum)
+
+}
+
+// for...of loop
+
+function miniMaxSum(arr) {
+    let total = 0;
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let num of arr) {
+        total += num;
+
+        if (num < min) {
+            min = num;
+        }
+
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    const minSum = total - max;
+    const maxSum = total - min;
+
+    console.log(minSum, maxSum);
+}
